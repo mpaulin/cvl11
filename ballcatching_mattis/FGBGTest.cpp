11f591053e0747995effe53dae493fa36f6d47fc
Mattis <mattis@linux.local>
2011-12-04 14:01:16 +0100

    +blob tracking

--- ballcatching_mattis/FGBGTest.cpp
+++ ballcatching_mattis/FGBGTest.cpp
@@ -1,6 +1,7 @@
 #include "config.hpp"
 #include "blobDetection.hpp"
-
+#include "blobTracking.hpp"
+#include <time.h>
 
 double thresh = 150; 
 void threshold(Mat& image){
@@ -22,9 +23,11 @@ int main(int argc, char** argv){
   
   namedWindow("Image",1);
   namedWindow("FG",1);
+  //namedWindow("CC",1);
   VideoCapture cap;
   cap.open(string(argv[1]));
   if(!cap.isOpened()) return -1;
+  Trajectories trajectories;
   for(int i = 0;i<2000;i++){
     cout << "Read image " << i << endl;
     Mat frame;
@@ -35,12 +38,51 @@ int main(int argc, char** argv){
     FGDetector->Process(new IplImage(frame));
     Mat mask = Mat(FGDetector->GetMask());
     imshow("FG",mask);
+    
+    //clock_t t = clock();
     vector<Ellipse> blobs = getBlobs(mask);
-    for(unsigned int i = 0;i<blobs.size();i++){
-      ellipse(frame,blobs[i].center,Size(blobs[i].a,blobs[i].b),0,0,360,Scalar(255,0,0));
+    cout << "Found " << blobs.size() << " blobs" << endl;
+    //clock_t t2 = clock();
+    //cout << " Elapsed time : "<< double(t2-t)/CLOCKS_PER_SEC << endl;
+    //imshow("CC",clustersToImage(cluster(mask),frame.cols,frame.rows));
+    
+
+
+   
+    
+    int initFrames = 42;
+    if(i<initFrames){
+      trajectories = initTrajectories(blobs,frame);
     }
+    else{
+      vector<pair<Ellipse,int> > stampedBlobs;
+      for(vector<Ellipse>::const_iterator it = blobs.begin();it!=blobs.end();it++){
+	stampedBlobs.push_back(pair<Ellipse,int>(*it,i));
+      }
+      
+      updateTrajectories(trajectories,stampedBlobs,frame);
+     
+     
+      cout <<"Trajectory size : " <<  trajectories.trajectories.size() << " First trajectory size " << trajectories.trajectories[0].size() << endl;
+    } 
+   
+  
+    
+    ///////////////////////////////////////////////
+    //              Rendering part               
+    ///////////////////////////////////////////////
+    
+    
+    
+
+    for(unsigned int j = 0;j<blobs.size();j++){
+      ellipse(frame,blobs[j].center,Size(blobs[j].a,blobs[j].b),0,0,360,Scalar(255,0,0));
+    } 
+    renderTrajectories(trajectories,frame);
     imshow("Image",frame);
-    if(waitKey(30) >= 0) break;
+
+
+    if(waitKey(40) >= 0) break;
   }
   return 0;
 }
